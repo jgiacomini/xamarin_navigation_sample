6f1a2cb4f5b0ea76ad4e0f06d50bc9db6b810ef5
jgiacomini <archeone@gmail.com>
2017-02-06 20:43:00 +0100

    Ajout du code W10 (Michel) Modif Android (Maxime)

---
+++ Sample.UWP/SecondaryPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Sample.UWP
+{
+    public sealed partial class SecondaryPage : Page
+    {
+        public SecondaryPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Tuple<string, string> param = e.Parameter as Tuple<string, string>;
+            if (param != null)
+            {
+                txtLabel.Text = param.Item1;
+                txtDescription.Text = param.Item2;
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void btnNavigation_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
