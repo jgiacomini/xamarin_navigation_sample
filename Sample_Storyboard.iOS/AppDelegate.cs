ffd9cdb7cae71e8d999af779cd83f5742c70d32f
jgiacomini <archeone@gmail.com>
2017-02-06 23:44:51 +0100

    Modification du AppDelegate

--- Sample_Storyboard.iOS/AppDelegate.cs
+++ Sample_Storyboard.iOS/AppDelegate.cs
@@ -3,37 +3,24 @@ using UIKit;
 
 namespace Sample_Storyboard.iOS
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the
-	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
 	[Register("AppDelegate")]
 	public class AppDelegate : UIApplicationDelegate
 	{
-		// class-level declarations
-
-		public override UIWindow Window
-		{
-			get;
-			set;
-		}
-
-		public static UIViewController initialViewController;
-
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
 			// Création du storyboard
 			UIStoryboard storyboard = UIStoryboard.FromName("MainStoryboard", null);
 
 			// Création de la fenêtre principale
-			Window = new UIWindow(UIScreen.MainScreen.Bounds);
+			var window = new UIWindow(UIScreen.MainScreen.Bounds);
 
 			// Création du ViewController initial
 			var mainViewController = storyboard.InstantiateInitialViewController() as UIViewController;
 
-			Window.RootViewController = initialViewController;
+			Window.RootViewController = mainViewController;
 			Window.MakeKeyAndVisible();
 			return true;
 		}
-
 	}
 }
 
