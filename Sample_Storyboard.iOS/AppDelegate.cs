1bf3348536a5f3a290239aae5cf39da80e6add02
jgiacomini <ismac@ismacs-Mac.local>
2017-05-22 16:30:21 +0200

    Small fix
    Small fix

--- Sample_Storyboard.iOS/AppDelegate.cs
+++ Sample_Storyboard.iOS/AppDelegate.cs
@@ -3,24 +3,32 @@ using UIKit;
 
 namespace Sample_Storyboard.iOS
 {
-	[Register("AppDelegate")]
-	public class AppDelegate : UIApplicationDelegate
-	{
-		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+
+
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+		public override UIWindow Window
 		{
-			// Création du storyboard
-			UIStoryboard storyboard = UIStoryboard.FromName("MainStoryboard", null);
-
-			// Création de la fenêtre principale
-			var window = new UIWindow(UIScreen.MainScreen.Bounds);
-
-			// Création du ViewController initial
-			var mainViewController = storyboard.InstantiateInitialViewController() as UIViewController;
-
-			Window.RootViewController = mainViewController;
-			Window.MakeKeyAndVisible();
-			return true;
+			get;
+			set;
 		}
-	}
+
+		public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            // Création du storyboard
+            UIStoryboard storyboard = UIStoryboard.FromName("Main", null);
+
+            // Création de la fenêtre principale
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            // Création du ViewController initial
+            var mainViewController = storyboard.InstantiateInitialViewController();
+
+            Window.RootViewController = mainViewController;
+            Window.MakeKeyAndVisible();
+            return true;
+        }
+    }
 }
 
--- Sample_Storyboard.iOS/MainViewController.designer.cs
+++ Sample_Storyboard.iOS/MainViewController.designer.cs
@@ -1,6 +1,6 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
+// This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
@@ -14,5 +14,8 @@ namespace Sample_Storyboard.iOS
     [Register ("MainViewController")]
     partial class MainViewController
     {
+        void ReleaseDesignerOutlets ()
+        {
+        }
     }
 }
